1121cc8e59dd7cd7e068a45c35069c123e9f7a94
jpalbarico <jpalbarico@up.edu.ph>
2020-12-11 18:42:50 +0800

    added main menu

---
+++ VR_Cardboard_Tests/Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("01");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
